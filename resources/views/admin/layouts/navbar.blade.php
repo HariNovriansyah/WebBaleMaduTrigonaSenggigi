54942dfa8b77f9964e733300110cddff4e04aa99
Naufal Rifqi Zuhrian <estarossa.nrz@gmail.com>
2024-07-02 04:14:13 +0800

    Reset Password & update profile
    Done html only

---
+++ resources/views/admin/layouts/navbar.blade.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+        <li><a href="{{ route('blogs.index') }}">Blogs</a></li>
+        <li><a href="{{ route('products.index') }}">Products</a></li>
+        <li><a href="{{ route('profile.edit') }}">Profile</a></li>
+    </ul>
+</nav>
